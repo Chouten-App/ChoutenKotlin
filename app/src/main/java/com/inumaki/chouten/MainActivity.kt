b310f099e41d875a710329d7b4a297f3ec5ab642
inumaki <kempcompany05@gmail.com>
2025-02-07 11:18:18 +0100

    ✨(feat): Navbar customization

--- app/src/main/java/com/inumaki/chouten/MainActivity.kt
+++ app/src/main/java/com/inumaki/chouten/MainActivity.kt
@@ -1,52 +1,279 @@
 package com.inumaki.chouten
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.inumaki.chouten.Features.DiscoverView
 import com.inumaki.chouten.ui.theme.ChoutenTheme
 
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeAmount: Int? = null
+)
+
+fun Modifier.topBorder(
+    color: Color,
+    height: Float,
+) = this.drawWithContent {
+    drawContent()
+    drawLine(
+        color = color,
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+        strokeWidth = height,
+    )
+}
+
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val systemUiController = rememberSystemUiController()
+
+            // Set the status bar and navigation bar colors
+            systemUiController.setSystemBarsColor(
+                color = Color(0xFF0C0C0C)
+            )
+
+            systemUiController.setNavigationBarColor(
+                color = Color(0xFF171717)
+            )
+
+            // setting up the individual tabs
+            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val alertsTab = TabBarItem(title = "Discover", selectedIcon = Icons.Filled.Info, unselectedIcon = Icons.Outlined.Info)
+            val settingsTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+
+            // creating a list of all the tabs
+            val tabBarItems = listOf(homeTab, alertsTab, settingsTab)
+
+            // creating our navController
+            val navController = rememberNavController()
+
             ChoutenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFF0C0C0C)
+                ) {
+                    Scaffold(bottomBar = { TabView(tabBarItems, navController) }) {
+                        NavHost(navController = navController, startDestination = homeTab.title) {
+                            composable(homeTab.title) {
+                                Column(
+                                    modifier = Modifier.fillMaxSize()
+                                        .background(Color(0xFF0C0C0C)),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        homeTab.title,
+                                    )
+                                }
+                            }
+                            composable(alertsTab.title) {
+                                DiscoverView()
+                            }
+                            composable(settingsTab.title) {
+                                Column(
+                                    modifier = Modifier.fillMaxSize()
+                                        .background(Color(0xFF0C0C0C)),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        settingsTab.title,
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+// ----------------------------------------
+// This is a wrapper view that allows us to easily and cleanly
+// reuse this component in any future project
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TabView(tabBarItems: List<TabBarItem>, navController: NavController) {
+    var selectedTabIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar(
+        containerColor = Color(0xFF171717),
+        modifier = Modifier.topBorder(
+            color = Color(59, 59, 59, 255),
+            height = 1f
+        )
+    ) {
+        // looping over each tab to generate the views and navigation for each item
+        tabBarItems.forEachIndexed { index, tabBarItem ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    navController.navigate(tabBarItem.title)
+                },
+                icon = {
+                    TabBarIconView(
+                        isSelected = selectedTabIndex == index,
+                        selectedIcon = tabBarItem.selectedIcon,
+                        unselectedIcon = tabBarItem.unselectedIcon,
+                        title = tabBarItem.title,
+                        badgeAmount = tabBarItem.badgeAmount
+                    )
+                },
+                label = {Text(tabBarItem.title)},
+                colors = NavigationBarItemColors(
+                    selectedTextColor = Color(0.8313725f, 0.8313725f, 0.8313725f, 1.0f),
+                    selectedIconColor = Color(0.8313725f, 0.8313725f, 0.8313725f, 1.0f),
+                    unselectedTextColor = Color(0.8313725f, 0.8313725f, 0.8313725f, 0.7f),
+                    unselectedIconColor = Color(0.8313725f, 0.8313725f, 0.8313725f, 0.7f),
+                    selectedIndicatorColor = Color.Transparent,
+                    disabledIconColor = Color.Transparent,
+                    disabledTextColor = Color.Transparent,
+                )
+            )
+        }
+    }
 }
 
+// This component helps to clean up the API call from our TabView above,
+// but could just as easily be added inside the TabView without creating this custom component
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TabBarIconView(
+    isSelected: Boolean,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    title: String,
+    badgeAmount: Int? = null
+) {
+    BadgedBox(badge = { TabBarBadgeView(badgeAmount) }) {
+        Icon(
+            imageVector = if (isSelected) {selectedIcon} else {unselectedIcon},
+            contentDescription = title
+        )
+    }
+}
+
+// This component helps to clean up the API call from our TabBarIconView above,
+// but could just as easily be added inside the TabBarIconView without creating this custom component
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun TabBarBadgeView(count: Int? = null) {
+    if (count != null) {
+        Badge {
+            Text(count.toString())
+        }
+    }
+}
+// end of the reusable components that can be copied over to any new projects
+// ----------------------------------------
+
+// This was added to demonstrate that we are infact changing views when we click a new tab
+@Composable
+fun MoreView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Thing 1")
+        Text("Thing 2")
+        Text("Thing 3")
+        Text("Thing 4")
+        Text("Thing 5")
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+    val alertsTab = TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications, badgeAmount = 7)
+    val settingsTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+    val moreTab = TabBarItem(title = "More", selectedIcon = Icons.Filled.List, unselectedIcon = Icons.Outlined.List)
+
+    // creating a list of all the tabs
+    val tabBarItems = listOf(homeTab, alertsTab, settingsTab, moreTab)
+
+    val navController = rememberNavController()
+
     ChoutenTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Scaffold(bottomBar = { TabView(tabBarItems, navController) }) {
+                NavHost(navController = navController, startDestination = homeTab.title) {
+                    composable(homeTab.title) {
+                        Text(homeTab.title)
+                    }
+                    composable(alertsTab.title) {
+                        Text(alertsTab.title)
+                    }
+                    composable(settingsTab.title) {
+                        Text(settingsTab.title)
+                    }
+                    composable(moreTab.title) {
+                        MoreView()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/inumaki/chouten/ui/theme/Theme.kt
+++ app/src/main/java/com/inumaki/chouten/ui/theme/Theme.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -40,6 +42,7 @@ fun ChoutenTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -56,3 +59,4 @@ fun ChoutenTheme(
         content = content
     )
 }
+
